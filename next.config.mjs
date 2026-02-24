e3f65582515a4ca4d7768718c3dcd954899ba3d0
touhidul7 <touhidul2@hotmail.com>
2026-02-24 06:20:45 +0600

    update

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://brittopos.brittosoft.site",
+        hostname: "ecommerce.skinvee.org",
         // port: "8000",
         pathname: "/storage/**",
       },
